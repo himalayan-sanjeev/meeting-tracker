1d0eec997532097b1b71ff11a39fa87afa7476a6
Sanjeev <sanjeevpoudel111@gmail.com>
2022-08-04 22:15:06 +0545

    Add user_id and status column to meetings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_090653) do
+ActiveRecord::Schema.define(version: 2022_08_04_162112) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "meetings", force: :cascade do |t|
     t.string "name"
@@ -18,6 +21,9 @@ ActiveRecord::Schema.define(version: 2022_08_04_090653) do
     t.datetime "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.integer "status", default: 0
+    t.index ["user_id"], name: "index_meetings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_08_04_090653) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "meetings", "users"
 end
