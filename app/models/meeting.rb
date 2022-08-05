66f1f01692efe20d87fa9899eee5bb2dbcc81633
Sanjeev <sanjeevpoudel111@gmail.com>
2022-08-05 22:48:42 +0545

    User Time.zone instaed of DateTime

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,23 +1,33 @@
 class Meeting < ApplicationRecord
   belongs_to :user
-
-  enum status: { upcoming: 0, ongoing: 1, completed:2, cancled:3 }
-
+  enum status: { upcoming: 0, ongoing: 1, completed:2, canceled:3 }
+  # after_update :meeting_status_update
+  
   def upcoming?
-    self.start_time > DateTime.now
-  end
-
-  def ongoing?
-    DateTime.now > self.start_time && DateTime.now < self.end_time
-  end
-
-  def completed?
-    self.end_time < DateTime.now
+    self.start_time > Time.zone.now
   end
   
-  def meeting_status_update
-    self.update(status:0) if self.upcoming?
-    self.update(status:1) if self.ongoing?
-    self.update(status:2) if self.completed?
+  def ongoing?
+    Time.zone.now > self.start_time && Time.zone.now < self.end_time
   end
+  
+  def completed?
+    self.end_time < Time.zone.now
+  end
+  
+  def update_meeting_status
+    if self.upcoming?
+      self.update(status:0)
+    elsif self.ongoing?
+      self.update(status:1) 
+    elsif self.completed?
+      self.update(status:2) 
+    end
+  end
+  
+  # def meeting_reminder
+  #   if self.start_time == Time.zone.now..Time.zone.now + 1.hour
+  #     MeetingReminderMailer.remind_meeting(self).deliver!
+  #   end
+  # end
 end
